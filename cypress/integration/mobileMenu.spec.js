0d13dc122019e39181b8c88072829c9812b40839
Fakiolas <m.fakiolas@hotmail.gr>
2020-06-01 02:03:55 +0300

    Add more test cases for mobile menu

--- cypress/integration/mobileMenu.spec.js
+++ cypress/integration/mobileMenu.spec.js
@@ -23,3 +23,53 @@ describe('a user who is in home page and uses a mobile device', () => {
     cy.url().should('include', '/contact');
   });
 });
+
+describe('a user who is in about page and uses the desktop', () => {
+  beforeEach(() => {
+    cy.viewport('iphone-x');
+  });
+
+  it('should be able to navigate to home page after clicking the logo in navbar', () => {
+    cy.visit('/about');
+
+    cy.get('nav a').contains('Logo').click();
+
+    cy.contains('h1', 'Home');
+    cy.url().should('include', '/');
+  });
+
+  it('should be able to navigate to contact page after clicking the about link in mobile menu', () => {
+    cy.visit('/about');
+
+    cy.get('[aria-label="mobile-toggle"]').click();
+    cy.get('[data-test="mobile-menu"] a').contains('Contact').click();
+
+    cy.contains('h1', 'Contact');
+    cy.url().should('include', '/contact');
+  });
+});
+
+describe('a user who is in contact page and uses the desktop', () => {
+  beforeEach(() => {
+    cy.viewport('iphone-x');
+  });
+
+  it('should be able to navigate to home page after clicking the logo in navbar', () => {
+    cy.visit('/contact');
+
+    cy.get('nav a').contains('Logo').click();
+
+    cy.contains('h1', 'Home');
+    cy.url().should('include', '/');
+  });
+
+  it('should be able to navigate to about page after clicking the about link in mobile menu', () => {
+    cy.visit('/contact');
+
+    cy.get('[aria-label="mobile-toggle"]').click();
+    cy.get('[data-test="mobile-menu"] a').contains('About').click();
+
+    cy.contains('h1', 'About');
+    cy.url().should('include', '/about');
+  });
+});
